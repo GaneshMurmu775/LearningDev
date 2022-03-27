7df759f5ba04110e2932ff40fe5c6538a02898c5
Ganesh Murmu <ganeshmurmu775@gmail.com>
2022-03-28 01:26:10 +0530

    string slice methode

--- module2/intro.js
+++ module2/intro.js
@@ -1,34 +1,6 @@
-let arr2 = [1, 7.2, 'a', "Ganesh Murmu", true];
+let string = "We are the champions";
 
-//Push Methode
-console.log("arr2 before push: " + arr2);
+//slice methode
 
-arr2.push("New Item");
-console.log("arr2 after push: " + arr2);
-
-//Pop Methode
-console.log("arr2 before pop: " + arr2);
-
-arr2.pop("New Item");
-console.log("arr2 after pop: " + arr2); 
-
-//shift methode
-console.log("arr2 before shift: " + arr2);
-
-arr2.shift();
-console.log("arr2 after shift: " + arr2);
-
-//Unshift methode
-console.log("arr2 before unshift: " + arr2);
-
-arr2.unshift("Mike");
-console.log("arr2 after unshift: " + arr2);
-
-console.log(arr2.length);
-
-
-
-
-
-
-
+console.log(string.slice(3, 5)); // 3 is starting index 
+// 5 is last index and is exclusive
