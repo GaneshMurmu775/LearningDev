92032a9bd4becc811538d263d2b4530bd583476f
Ganesh Murmu <ganeshmurmu775@gmail.com>
2022-03-28 01:17:50 +0530

    methods of array

--- module2/intro.js
+++ module2/intro.js
@@ -1,14 +1,34 @@
-//Empty array
-let arr = [];
-console.log(arr);
+let arr2 = [1, 7.2, 'a', "Ganesh Murmu", true];
 
-// ---------------------------------
+//Push Methode
+console.log("arr2 before push: " + arr2);
 
-let arr2 = [1, 1.2, 'a', "Ganesh Murmu", true];
-console.log(arr2);
+arr2.push("New Item");
+console.log("arr2 after push: " + arr2);
 
-console.log("Element at index 2: " + arr2[2]);
+//Pop Methode
+console.log("arr2 before pop: " + arr2);
 
-console.log("Replacing a with b");
-arr2[2] = 'b';
-console.log(arr2);
+arr2.pop("New Item");
+console.log("arr2 after pop: " + arr2); 
+
+//shift methode
+console.log("arr2 before shift: " + arr2);
+
+arr2.shift();
+console.log("arr2 after shift: " + arr2);
+
+//Unshift methode
+console.log("arr2 before unshift: " + arr2);
+
+arr2.unshift("Mike");
+console.log("arr2 after unshift: " + arr2);
+
+console.log(arr2.length);
+
+
+
+
+
+
+
