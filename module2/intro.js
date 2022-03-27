48dfa9ab665f200ab94c21f428e83d97d139221c
Ganesh Murmu <ganeshmurmu775@gmail.com>
2022-03-28 00:53:50 +0530

    IIFE with arguments

--- module2/intro.js
+++ module2/intro.js
@@ -1,7 +1,7 @@
 // IIFE - Immediately Invoked Function Expression
 (
-    function()
+    function(num1, num2)
     {
-        console.log("Hello world");
+        console.log(10 + 5);
     }
-) ();
+) (10, 5);
