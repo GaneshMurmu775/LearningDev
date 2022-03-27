1f0c71fd1fc2294c95869b38821f7c82da866047
Ganesh Murmu <ganeshmurmu775@gmail.com>
2022-03-28 01:51:40 +0530

    split methode

--- module2/intro.js
+++ module2/intro.js
@@ -1,11 +1,4 @@
-let firstName = "Ganesh ";
-let lastName = "Murmu";
+let string = "I am a winner";
 
-//concat methode
-console.log(firstName.concat(lastName));
-
-// another way of concatenation
-let fn = "Ben";
-let ln = "Tenision";
-let fullName = fn + ln;
-console.log(fullName);
+let arr = string.split(" ");
+console.log(arr);
