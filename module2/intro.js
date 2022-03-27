8e91129f712f02005df7cd39e2c91ec82c708d6b
Ganesh Murmu <ganeshmurmu775@gmail.com>
2022-03-28 01:02:44 +0530

    Basics of array

--- module2/intro.js
+++ module2/intro.js
@@ -1,7 +1,14 @@
-// IIFE - Immediately Invoked Function Expression
-(
-    function(num1, num2)
-    {
-        console.log(10 + 5);
-    }
-) (10, 5);
+//Empty array
+let arr = [];
+console.log(arr);
+
+// ---------------------------------
+
+let arr2 = [1, 1.2, 'a', "Ganesh Murmu", true];
+console.log(arr2);
+
+console.log("Element at index 2: " + arr2[2]);
+
+console.log("Replacing a with b");
+arr2[2] = 'b';
+console.log(arr2);
