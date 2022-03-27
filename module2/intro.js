40a92d1d7c924003958b01b62002c2ff251f34d1
Ganesh Murmu <ganeshmurmu775@gmail.com>
2022-03-28 00:49:22 +0530

    Immediately Invoked Function Expression

--- module2/intro.js
+++ module2/intro.js
@@ -1,7 +1,7 @@
-let a = 
-function add(num1, num2)
-{
-    return num1 + num2;
-}
-
-console.log(a(5, 14));
+// IIFE - Immediately Invoked Function Expression
+(
+    function()
+    {
+        console.log("Good Night world");
+    }
+) ();
