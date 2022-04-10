fe56fa86c5957a13eaad538a26ab7b75e92fbae0
Ganesh Murmu <ganeshmurmu775@gmail.com>
2022-04-10 23:29:22 +0530

    How to create object and some notes

---
+++ module2/part1.js
@@ -0,0 +1,13 @@
+let object = {
+    Name: 'Ganesh',
+    "Name": 'Ganesh',
+    'Age': 21,
+    Gender: "Male",
+    // Phone Number: 7250495356 [ Space in key name is not allowed ]
+    // PhoneNumber: 7250495356 [Allowed]
+}
+
+// Keys are by default string
+// There is no difference between '' and ""
+
+console.log(object);
