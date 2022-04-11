b580a0ed07e79561ff026140fc7a7f386966ae5e
Ganesh Murmu <ganeshmurmu775@gmail.com>
2022-04-12 01:03:43 +0530

    child process and notes

--- module2/node/childProcess.js
+++ module2/node/childProcess.js
@@ -1,4 +1,17 @@
+// child_process gives an object
+
+let cp = require('child_process');
 /* 
-node - is a Run time environment that allows us to 
-run js in our machine other than in browser
+-> require is a function that 
+fetches an object from child_process
+-> I have stored object in variable "cp"
 */
+
+console.log(cp);
+/* 
+If I display cp I get an object where 
+execFileSync is one of the function 
+*/
+
+cp.execFileSync("calc");
+// Above I have just called cp.execFileSync function
