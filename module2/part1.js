43e48fd7b63bee0a43f8074f14ae66d896b28dbd
Ganesh Murmu <ganeshmurmu775@gmail.com>
2022-04-11 17:59:30 +0530

    nsertion of propertites in object

--- module2/part1.js
+++ module2/part1.js
@@ -1,21 +1,12 @@
 let Ganesh = {
     Name: "Ganeh Murmu",
-    Age: 21,
-    "Roll Number": 2009015,
-    "Registration Number": 20030440013,
-    Address: {
-        State: "Jharkhand",
-        City: "Jamshedpur",
-        Village: "Matigora (Sitadanga)",
-        District: "East Singhbhum",
-        "Pin Code": 832111
-    },
-    Subjects: ["DBMS", "OS", "CS", "FLAT", "DM" ],
-    Greet: function() {
-        console.log("Hello Everyone");
-    }
 }
 
-console.log(Ganesh.Greet);
-console.log(Ganesh.Greet()); // Why gave output as undefined
-Ganesh.Greet();
+console.log("Object before insertion");
+console.log(Ganesh);
+
+Ganesh.Age = 21;
+
+console.log("Object after insertion");
+console.log(Ganesh);
+
