273f46b69916cb0be1bf1ca88b0e0b4626276fa7
Ganesh Murmu <ganeshmurmu775@gmail.com>
2022-05-06 19:14:55 +0530

    __filename and __dirname

--- module2/node/path.js
+++ module2/node/path.js
@@ -1,13 +1,7 @@
 const { extname } = require("path");
 let path = require("path");
 
-// extname() function takes path of a file
-// as a string as an argument and returns file extension 
-let extensionName = path.extname("C:\Users\ganes\Desktop\Learning\FJP\DEV\module2\node\path.js");
-console.log(extensionName);
-
-// baseame() function takes path of a file
-// as a string as an argument and returns file name
-// but below code is not working
-let baseName = path.basename("C:\Users\ganes\Desktop\Learning\FJP\DEV\module2\node\path.js");
+// __filename -> returns file path
+// __dirname -> return dir path
+let baseName = path.basename(__filename);
 console.log(baseName);
