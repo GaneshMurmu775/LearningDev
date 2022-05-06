49bd98df4c9e5f7e689536e5babbed7fde2ddc31
Ganesh Murmu <ganeshmurmu775@gmail.com>
2022-05-06 20:26:49 +0530

    file.txt modified using appendFileSync

--- module2/node/fs.js
+++ module2/node/fs.js
@@ -5,12 +5,8 @@ let newFilePath = path.join(__dirname, "file.txt");
 
 fs.writeFileSync(newFilePath, "I am created using js");
 
+// ***appendFileSync Function
+fs.appendFileSync(newFilePath, "\nI am a text appended in file.txt")
 
-/* 
-readFileSync function -> reads file,
-takes two or one arguments
-1) path of the file
-2) encoding 'utf-8' (optional)   
-*/
 let content = fs.readFileSync(newFilePath, 'utf-8');
 console.log(content);
