236a513571bfb54db88b8d96c4d67479c98f2005
Ganesh Murmu <ganeshmurmu775@gmail.com>
2022-05-06 19:42:52 +0530

    join function

--- module2/node/path.js
+++ module2/node/path.js
@@ -1,7 +1,16 @@
 const { extname } = require("path");
 let path = require("path");
 
-// __filename -> returns file path
-// __dirname -> return dir path
-let baseName = path.basename(__filename);
-console.log(baseName);
+
+
+// join function
+// Task -> I wana add a file name in a directory
+let newFile1 = __dirname + "\\test2.js";
+// Above is one way, but I had to know the style of directory name
+// Problem -> I don't know what is the style of directory name,
+// it could be any of these way -> // / \
+// therefor join function let us do our task just by writing file name
+let newFile2 = path.join(__dirname, "test3.js");
+
+console.log(newFile1);
+console.log(newFile2);
