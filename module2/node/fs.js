68938554c079435efbda272b5df39d96f943f3ef
Ganesh Murmu <ganeshmurmu775@gmail.com>
2022-05-06 20:29:01 +0530

    file.txt deleted using unlinkSync() function

--- module2/node/fs.js
+++ module2/node/fs.js
@@ -3,10 +3,4 @@ let path = require("path");
 
 let newFilePath = path.join(__dirname, "file.txt");
 
-fs.writeFileSync(newFilePath, "I am created using js");
-
-// ***appendFileSync Function
-fs.appendFileSync(newFilePath, "\nI am a text appended in file.txt")
-
-let content = fs.readFileSync(newFilePath, 'utf-8');
-console.log(content);
+fs.unlinkSync(newFilePath);
